74f12ba9d34ca5bf36908456847d91bc46f2d312
hasunB <hasunbandara17@gmail.com>
2025-01-05 22:02:51 +0530

    designing the front-end

--- src/radianpro/FXMLDocumentController.java
+++ src/radianpro/FXMLDocumentController.java
@@ -10,25 +10,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 /**
  *
  * @author hasun
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private ImageView myImageView;
+
+    public void exit() {
+        System.exit(0);
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
 }
--- src/radianpro/RadianPro.java
+++ src/radianpro/RadianPro.java
@@ -5,10 +5,18 @@
 package radianpro;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -16,11 +24,42 @@ import javafx.stage.Stage;
  */
 public class RadianPro extends Application {
     
+    private double  x = 0;
+    private double  y = 0;
+    
+    
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
+        root.setOnMousePressed(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                
+                x = event.getSceneY();
+                y = event.getSceneY();
+                
+            }
+        });
+        
+        root.setOnMouseDragged(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                
+                stage.setX(event.getScreenX() - x);
+                stage.setY(event.getScreenY() - y);
+                
+            }
+        });
+ 
+        
+        
         Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         
         stage.setScene(scene);
         stage.show();
